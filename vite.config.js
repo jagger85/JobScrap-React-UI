55370fc96d2f27321d6f0fa1db2414dfb6e335cb
jagger85 <jagger85.crypto@gmail.com>
2024-11-12 12:14:39 +0000

    Added custom folder for builds in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build:{
+	outDir:'/var/www/client'
+  },
   plugins: [react()],
   server: {
     host: '0.0.0.0',
