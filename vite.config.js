7ab0879d116db26a91006dc3f40dc8867145923b
Jagger85 <andresfonsortega@yahoo.es>
2024-11-16 09:44:14 +0000

    Edit vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build:{
-	outDir:'/var/www/client'
+  build: {
+    outDir: 'dist', // Change this to 'dist' for CI/CD
+    emptyOutDir: true,
   },
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000, // You can change this port if needed
+    port: 3000,
   },
 })
