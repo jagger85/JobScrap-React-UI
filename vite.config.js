9eaa615c0706ef179a4669b6f0b8728ffe059cd2
Jagger85 <andresfonsortega@yahoo.es>
2024-11-16 09:41:38 +0000

    Edit vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000, // You can change this port if needed
+    port: 3000,
   },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    assetsDir: 'assets',
+    manifest: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  }
 })
