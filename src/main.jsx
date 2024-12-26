d519550516a3ec683d32c54c8a01008c5586846e
Jagger85 <andresfonsortega@yahoo.es>
2024-12-26 16:05:11 +0700

    Chore(main.jsx): Removed unused context

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-
 import { AuthContextProvider } from './contexts/AuthContext.jsx'
-import { PlatformsContextProvider } from './contexts/PlatformsContext.jsx'
 import { ConnectionContextProvider } from './contexts/ConnectionContext.jsx'
 import App from './App.jsx'
 import './global.css'
@@ -12,11 +10,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
         <AuthContextProvider>
-          <PlatformsContextProvider>
             <ConnectionContextProvider>
                 <App />
             </ConnectionContextProvider>
-          </PlatformsContextProvider>
         </AuthContextProvider>
     </BrowserRouter>
   </StrictMode>
