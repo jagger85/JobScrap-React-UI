fa7ca3f6538d80158dda833f7313a317ad0463f9
Jagger85 <andresfonsortega@yahoo.es>
2024-12-31 18:20:04 +0700

    Fix(Collapsable panel): coded a fixed size for platform icon

--- src/components/Panels/CollapsablePanel.jsx
+++ src/components/Panels/CollapsablePanel.jsx
@@ -19,7 +19,7 @@ export default function CollapsablePanel(props) {
       >
 
         <div className='cp-panel-header-data'>
-        <img src={operation.icon}/>
+        <img src={operation.icon} style={{ width: '30px', height: '30px' }} />
         <div className="cp-panel-header-item"><span className="cp-panel-header-item-platform">{operation.platform}</span></div>
         <div className="cp-panel-header-item">Keywords: <span>{operation.keywords}</span></div>
         <div className="cp-panel-header-item">Date range: <span>{operation.dateRange}</span></div>
