e982b8393fbfcf0c2b8ef9da319bc8027342aaed
Jagger85 <andresfonsortega@yahoo.es>
2025-01-02 18:40:40 +0700

    Feat(useApi): Added new requests for automatic scraping operations

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,6 +1,8 @@
-const BASE_URL = `http://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}/api`
+const BASE_URL = `http://${import.meta.env.VITE_BACKEND_HOST}:${
+  import.meta.env.VITE_BACKEND_PORT
+}/api`
 
-import useStorage from "./useStorage"
+import useStorage from './useStorage'
 
 const getBaseUrl = () => {
   if (import.meta.env.PROD) import.meta.env.VITE_API_URL
@@ -9,16 +11,17 @@ const getBaseUrl = () => {
 
 const getSocketUrl = () => {
   if (import.meta.env.PROD) import.meta.env.VITE_API_SOCKET
-  else return `ws://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}/api/socket`
+  else
+    return `ws://${import.meta.env.VITE_BACKEND_HOST}:${
+      import.meta.env.VITE_BACKEND_PORT
+    }/api/socket`
 }
 
+const useApi = () => {
+  const { getToken } = useStorage()
 
-const useApi = () =>{
-
-const { getToken } = useStorage()
-
- async function fetchUsers() {
-  const response = await fetch(`${BASE_URL}/users`, {
+  async function fetchUsers() {
+    const response = await fetch(`${BASE_URL}/users`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -29,7 +32,7 @@ const { getToken } = useStorage()
     return data
   }
 
-  async function addUser(username, password, role){
+  async function addUser(username, password, role) {
     const response = await fetch(`${BASE_URL}/users`, {
       method: 'POST',
       headers: {
@@ -39,13 +42,13 @@ const { getToken } = useStorage()
       body: JSON.stringify({
         username: username,
         password: password,
-        role: role
-      })
+        role: role,
+      }),
     })
     return response
   }
 
-  async function deleteUser(username){
+  async function deleteUser(username) {
     const response = await fetch(`${BASE_URL}/users/${username}`, {
       method: 'DELETE',
       headers: {
@@ -55,37 +58,40 @@ const { getToken } = useStorage()
     })
     return response
   }
-  
- async function login(username, password, rememberMe){
-    const response = await fetch(`${BASE_URL}/auth/login`,{
+
+  async function login(username, password, rememberMe) {
+    const response = await fetch(`${BASE_URL}/auth/login`, {
       method: 'POST',
-      headers:{
-        'Content-Type' : 'application/json',
+      headers: {
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         username: username,
         password: password,
-        remember_me: rememberMe
-      })
+        remember_me: rememberMe,
+      }),
     })
     return response
   }
-  async function changePassword(newPassword, username){
-    const response = await fetch(`${BASE_URL}/users/${username}/change-password`,{
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${getToken()}`,
-      },
-      body: JSON.stringify({
-        newPassword: newPassword,
-      })
-    })
+  async function changePassword(newPassword, username) {
+    const response = await fetch(
+      `${BASE_URL}/users/${username}/change-password`,
+      {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${getToken()}`,
+        },
+        body: JSON.stringify({
+          newPassword: newPassword,
+        }),
+      }
+    )
     return response
   }
-  
- async function validateToken(token){
-    const response = await fetch(`${BASE_URL}/auth/validate-token`,{
+
+  async function validateToken(token) {
+    const response = await fetch(`${BASE_URL}/auth/validate-token`, {
       mehtod: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -95,7 +101,7 @@ const { getToken } = useStorage()
     return response
   }
 
-  async function fetchOperations(){
+  async function fetchOperations() {
     const response = await fetch(`${BASE_URL}/operations`, {
       method: 'GET',
       headers: {
@@ -105,9 +111,9 @@ const { getToken } = useStorage()
     })
     const data = await response.json()
     return data
-    }
+  }
 
-  async function scrapOperationsByDateRange(keywords, dateRange, platform){
+  async function scrapOperationsByDateRange(keywords, dateRange, platform) {
     const response = await fetch(`${BASE_URL}/${platform}`, {
       method: 'POST',
       headers: {
@@ -117,14 +123,13 @@ const { getToken } = useStorage()
       body: JSON.stringify({
         keywords: keywords,
         days: dateRange,
-      })
+      }),
     })
     const data = await response.json()
     return data
   }
-  
 
-  async function deleteOperation(id){
+  async function deleteOperation(id) {
     const response = await fetch(`${BASE_URL}/operations/${id}`, {
       method: 'DELETE',
       headers: {
@@ -135,7 +140,7 @@ const { getToken } = useStorage()
     return response
   }
 
-  async function fetchOperationByTaskId(taskId){
+  async function fetchOperationByTaskId(taskId) {
     const response = await fetch(`${BASE_URL}/operations/task/${taskId}`, {
       method: 'GET',
       headers: {
@@ -146,8 +151,90 @@ const { getToken } = useStorage()
     const data = await response.json()
     return data
   }
-    
-  return {fetchUsers, login, validateToken, addUser, deleteUser, changePassword, fetchOperations, scrapOperationsByDateRange, deleteOperation, fetchOperationByTaskId}  
+
+  async function fetchAutomatedScrapOperations() {
+    const response = await fetch(`${BASE_URL}/automated_scrap_operations`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${getToken()}`,
+      },
+    })
+    const data = await response.json()
+    return data
+  }
+
+  async function createAutomatedScrapOperation(data) {
+    const response = await fetch(`${BASE_URL}/automated_scrap_operations`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${getToken()}`,
+      },
+      body: JSON.stringify(data),
+    })
+    return response
+  }
+
+  async function deleteAutomatedScrapOperation(id) {
+    const response = await fetch(
+      `${BASE_URL}/automated_scrap_operations/${id}`,
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${getToken()}`,
+        },
+      }
+    )
+    return response
+  }
+
+  async function activateAutomatedScrapOperation(id) {
+    const response = await fetch(
+      `${BASE_URL}/automated_scrap_operations/${id}/activate`,
+      {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${getToken()}`,
+        },
+      }
+    )
+    return response
+  }
+
+  async function deactivateAutomatedScrapOperation(id) {
+    const response = await fetch(
+      `${BASE_URL}/automated_scrap_operations/${id}/deactivate`,
+      {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${getToken()}`,
+        },
+      }
+    )
+    return response
+  }
+
+  return {
+    fetchUsers,
+    login,
+    validateToken,
+    addUser,
+    deleteUser,
+    changePassword,
+    fetchOperations,
+    scrapOperationsByDateRange,
+    deleteOperation,
+    fetchOperationByTaskId,
+    fetchAutomatedScrapOperations,
+    createAutomatedScrapOperation,
+    deleteAutomatedScrapOperation,
+    activateAutomatedScrapOperation,
+    deactivateAutomatedScrapOperation,
+  }
 }
 
 export const API_BASE_URL = getBaseUrl()
