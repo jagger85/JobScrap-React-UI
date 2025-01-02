95051ac5b574aaa84b93b3823882692c6248137f
Jagger85 <andresfonsortega@yahoo.es>
2025-01-02 18:41:03 +0700

    Feat(Automation): Created Automation Page

--- src/pages/Automation/Automation.jsx
+++ src/pages/Automation/Automation.jsx
@@ -1,57 +1,77 @@
-import kalibrrIcon from '../../assets/platform-icons/kalibrr.svg'
-import PlatformCard from '../../components/Platforms/PlatformCard'
 import PageLayout from '../../Layout/PageLayout'
-import Section from '../../components/Section/Section'
+import IconButton from '../../components/Buttons/IconButton'
+import { useState } from 'react'
+import AutomateScrapModal from '@components/Modals/AutomateScrap/AutomateScrapModal'
+import { CreateIcon } from '../../components/Icons'
+import useApi from '../../hooks/useApi'
+import AutomaticOperationPanel from '@components/Panels/AutomaticOperationPanel'
+import { useQuery } from '@tanstack/react-query'
+
 import './automation.css'
 function Automation() {
-  const platformCards = [
-    {
-      id: 0,
-      platform: 'Kalibrr',
-      icon: kalibrrIcon,
-      createdAt: '2 days ago',
-      keywords: 'python',
-      dateRange: 'every week',
-      lastRun: 'last week',
-    },
-    {
-      id: 1,
-      platform: 'Kalibrr',
-      icon: kalibrrIcon,
-      createdAt: '2 days ago',
-      keywords: 'python',
-      dateRange: 'every week',
-      lastRun: 'last week',
-    },
-    {
-      id: 2,
-      platform: 'Kalibrr',
-      icon: kalibrrIcon,
-      createdAt: '2 days ago',
-      keywords: 'python',
-      dateRange: 'every week',
-      lastRun: 'last week',
-    },
-  ]
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const {
+    fetchAutomatedScrapOperations,
+    createAutomatedScrapOperation,
+    deleteAutomatedScrapOperation,
+    activateAutomatedScrapOperation,
+    deactivateAutomatedScrapOperation,
+  } = useApi()
+  const { data, isError, error, isLoading, refetch } = useQuery({
+    queryKey: ['automatedScrapOperations'],
+    queryFn: fetchAutomatedScrapOperations,
+  })
+
+  const handleCreate = () => {
+    setIsModalOpen(true)
+  }
+  const handleClose = () => {
+    setIsModalOpen(false)
+  }
+  const addAutomatedOperation = async (data) => {
+    await createAutomatedScrapOperation(data)
+    refetch()
+  }
+  const handleDelete = async (operation) => {
+    await deleteAutomatedScrapOperation(operation)
+    refetch()
+  }
+  const handleActivate = async (operation) => {
+    await activateAutomatedScrapOperation(operation)
+    refetch()
+  }
+  const handleDeactivate = async (operation) => {
+    await deactivateAutomatedScrapOperation(operation)
+    refetch()
+  }
+
+  if (isLoading) return <h2>Loading...</h2>
+  if (isError) return <h2>Oooops something went wrong {error}</h2>
+
+
   return (
     <PageLayout title="Automation">
-      <Section title="Automatic Job Scraps">
-        <div className="automation-cards">
-          {platformCards.map((card) => {
-            return (
-              <PlatformCard
-                key={card.id}
-                platform={card.platform}
-                icon={card.icon}
-                createdAt={card.createdAt}
-                keywords={card.keywords}
-                dateRange={card.dateRange}
-                lastRun={card.lastRun}
-              />
-            )
-          })}
-        </div>
-      </Section>
+      <div className="scrap-header">
+        <IconButton icon={CreateIcon} onClick={handleCreate} type="rounded" />
+        <div className="scrap-header-subtitle">Create an automated scrap operation</div>
+      </div>
+      {data.map((operation) => {
+        return (
+          <AutomaticOperationPanel
+            key={operation._id}
+            operation={operation}
+            onDelete={handleDelete}
+            onActivate={handleActivate}
+            onDeactivate={handleDeactivate}
+          />
+        )
+      })}
+      {isModalOpen && (
+        <AutomateScrapModal
+          onClose={handleClose}
+          addAutomatedOperation={addAutomatedOperation}
+        />
+      )}
     </PageLayout>
   )
 }
