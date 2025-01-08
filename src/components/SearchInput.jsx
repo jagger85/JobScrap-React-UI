316e500fab48753d70f8c19572a965d1b7e0924e
Jagger85 <andresfonsortega@yahoo.es>
2025-01-08 11:19:17 +0700

    Feat(SearchInput): Created a search Input component

---
+++ src/components/SearchInput.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { SearchIcon } from '@icons'
+
+const inputStyle = {
+    paddingLeft: '40px',
+}
+const iconContainerStyle = {
+    position: 'relative',
+}
+const iconStyle = {
+    position: 'absolute',
+    left: '10px',
+    top: '50%',
+    transform: 'translateY(-50%)',
+    pointerEvents: 'none',
+}
+
+
+function SearchInput({ placeholder, onChange, value }) {
+    return (
+        <div style={iconContainerStyle}>
+            <input style={inputStyle} type="text" placeholder={placeholder} onChange={onChange} value={value} />
+            <SearchIcon style={iconStyle} />
+        </div>
+    )
+}
+
+
+
+export default SearchInput
