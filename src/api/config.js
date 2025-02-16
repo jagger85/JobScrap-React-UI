f4d1af44b84f8957a048b1ce7d4a7df6222707ee
Jagger85 <andresfonsortega@yahoo.es>
2025-02-16 15:31:56 +0700

    Fix(api_config): Fixed the urls construction

--- src/api/config.js
+++ src/api/config.js
@@ -1,16 +1,20 @@
 export const API_CONFIG = {
-    BASE_URL: import.meta.env.PROD 
-      ? import.meta.env.VITE_API_URL 
-      : `http://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}/api`,
-    
-    SOCKET_URL: import.meta.env.PROD
-      ? import.meta.env.VITE_API_SOCKET
-      : `ws://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}/api/socket`,
-  
-    ENDPOINTS: {
-      USERS: '/users',
-      AUTH: '/auth',
-      OPERATIONS: '/operations',
-      SCHEDULED_SCRAP: '/scheduled_scrap_operations',
-    }
-  }
+  BASE_URL:
+    import.meta.env.VITE_API_URL ||
+    `http://${import.meta.env.VITE_BACKEND_HOST}:${
+      import.meta.env.VITE_BACKEND_PORT
+    }/api`,
+
+  SOCKET_URL:
+    import.meta.env.VITE_API_SOCKET ||
+    `ws://${import.meta.env.VITE_BACKEND_HOST}:${
+      import.meta.env.VITE_BACKEND_PORT
+    }/api/socket`,
+
+  ENDPOINTS: {
+    USERS: '/users',
+    AUTH: '/auth',
+    OPERATIONS: '/operations',
+    SCHEDULED_SCRAP: '/scheduled_scrap_operations',
+  },
+}
